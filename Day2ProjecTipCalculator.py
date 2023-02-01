652055847002784352cbba54f3d89fe6cfd7f68a
sebaswhilesebas <113312604+sebaswhilesebas@users.noreply.github.com>
2023-02-01 12:44:42 -0300

    Update Day2ProjecTipCalculator.py

--- Day2ProjecTipCalculator.py
+++ Day2ProjecTipCalculator.py
@@ -4,12 +4,6 @@
 ##How many people to split the bill? 7
 #If the bill was $150.00, split between 5 people, with 12% tip. 
 
-#Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Format the result to 2 decimal places = 33.60
-
-#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
-
-#Write your code below this line 👇
 
 print("Welcome to the tip calculator!")
 cuenta= float(input("What was the total bill?\n$"))
@@ -18,6 +12,4 @@ personas= float(input("How many people to split the bill?\n"))
 
 print (f"Each person should pay: ${propina/100+1*cuenta/personas:0.2f}\n")
 
-##print (f"Each person should pay: ${cadaunopaga: .2f}\n")       
-#cadaunopaga= cuentamaspropina/personas                  
-#print ("Each person should pay"+cadaunopaga
+
